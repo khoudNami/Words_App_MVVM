65f543f6ad996e33863eb7e7001a421b69c69af0
Sibonelo Shezi <siboneloshezi50@gmail.com>
2020-11-14 11:37:32 +0200

    Task 6: Create the Repository, completed.

--- app/src/main/java/com/example/roomwordssample/WordDao.java
+++ app/src/main/java/com/example/roomwordssample/WordDao.java
@@ -1,11 +1,13 @@
 package com.example.roomwordssample;
 
 import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
 
+@Dao
 public interface WordDao {
 
     @Insert

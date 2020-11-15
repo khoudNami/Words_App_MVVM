883673bbe8ff2329a6d65fb95cf791cf8b7ba6ed
Sibonelo Shezi <siboneloshezi50@gmail.com>
2020-11-15 07:48:58 +0200

    Task 10: Populate the database completed

--- app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
+++ app/src/main/java/com/example/roomwordssample/WordRoomDatabase.java
@@ -1,10 +1,13 @@
 package com.example.roomwordssample;
 
 import android.content.Context;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
 @Database(entities = {Word.class}, version = 1, exportSchema = false)
 public abstract class WordRoomDatabase extends RoomDatabase {
@@ -13,6 +16,15 @@ public abstract class WordRoomDatabase extends RoomDatabase {
 
     private static WordRoomDatabase INSTANCE;
 
+    private static RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onOpen(@NonNull SupportSQLiteDatabase db) {
+                    super.onOpen(db);
+
+                }
+            };
+
     static WordRoomDatabase getInstance(final Context context) {
         if (INSTANCE == null) {
             synchronized (WordRoomDatabase.class) {
@@ -20,12 +32,33 @@ public abstract class WordRoomDatabase extends RoomDatabase {
                     INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
                             WordRoomDatabase.class, "words_database")
                             .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
                             .build();
                 }
             }
         }
 
         return INSTANCE;
+    }
+
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+        private final WordDao mDao;
+        String[] words = {"ubaba", "umama", "ingane"};
+
+        public PopulateDbAsync(WordRoomDatabase db) {
+            mDao = db.getWordDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            mDao.deleteAll();
+
+            for (int i = 0; i < words.length; i++) {
+                Word word = new Word(words[i]);
+                mDao.insert(word);
+            }
+            return null;
+        }
     }
 
 }

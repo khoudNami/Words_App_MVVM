4b855a41bab3ceae12996a3e8112c8ef42e41e9e
Sibonelo Shezi <siboneloshezi50@gmail.com>
2020-11-15 09:11:24 +0200

    fix task 12: add onConflict = OnConflictStrategy.IGNORE in @Insert

--- app/src/main/java/com/example/roomwordssample/WordDao.java
+++ app/src/main/java/com/example/roomwordssample/WordDao.java
@@ -3,13 +3,14 @@ package com.example.roomwordssample;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.List;
 @Dao
 public interface WordDao {
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insert(Word word);
 
     @Query("DELETE FROM word_table")
